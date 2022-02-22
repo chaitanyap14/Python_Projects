03886ec39ce9f198896134ee125c0a9327eabeae
Chaitanya Palghadmal <cpalghadmal14@gmail.com>
2022-02-22 10:53:44 +0530

    Guitar Fretboard Trainer Script

--- Guitar_Fretboard_Trainer.py
+++ Guitar_Fretboard_Trainer.py
@@ -2,6 +2,8 @@ import random
 
 print("===== FRETBOARD TRAINER =====")
 
+print("\nIf you can complete level 2, 3, and 4 at 100bpm, you're done.")
+
 notes = ["A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab"]
 
 
